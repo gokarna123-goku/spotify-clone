3fc2a850f3941f5eefe78bec1973ad34bccf92f9
Gokarna Adhikari <agokarna444@gmail.com>
2024-07-14 15:09:20 +0545

    complete the footer section

--- src/pages/main/content/Content.jsx
+++ src/pages/main/content/Content.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Footer from '../../../components/footer/Footer'
 
 const Content = () => {
     return (
-        <div className='flex-1 h-40 overflow-y-scroll overflow-x-hidden bg-neutral-800/30 rounded-md px-6 py-5'>
-            This is Content
+        <div className='flex-1 h-[calc(100vh-8.5ch)] overflow-y-scroll overflow-x-hidden bg-neutral-800/30 rounded-md px-6 py-5'>
+            {/* Navbar */}
+            {/* Popular artists */}
+            {/* Popular Albums */}
+            {/* Popular Radio */}
+            {/* Featured Charts */}
+            {/* Spotify Playlist */}
+            {/* Footer */}
+            <Footer />
         </div>
     )
 }
